daacb4b830954674edb809d8959e323439d62d68
Judy <jzhu@lcs.on.ca>
2026-02-23 14:24:54 -0500

    Initial Commit

---
+++ Clock App/Clock_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_AppApp.swift
+//  Clock App
+//
+//  Created by Judy Z on 2026/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Clock_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clock App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clock App
+//
+//  Created by Judy Z on 2026/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
